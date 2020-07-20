a39a753d6c730486d6e76c940777e46994925543
csbendercs <csbendercsms@gmail.com>
2020-07-19 22:31:58 -0400

    added bigger button and stuff

--- dice.js
+++ dice.js
@@ -8,26 +8,32 @@ var dice = {
   
   
   
-  //Prints dice roll to the page
-  
-  function printNumber(number) {
+//Prints dice roll to the page
+
+function printNumber(number) {
     var placeholder = document.getElementById('placeholder');
     var numDict = {
-        1: "+1",
+        1: "assets/tentacle.jpg",
         2: "+2",
         3: "0", 
         4: "-1",
         5: "-2"   
     };
+    randomNum = numDict[number]
+    if(number == 1) {
+        document.getElementById('picture').src = "assets/tentacle.jpg";
+    }
+    else {
+        placeholder.innerHTML = randomNum;
+        document.getElementById('picture').src = "";    
+    }
+}
 
-    placeholder.innerHTML = numDict[number];
-  }
-  
-  var button = document.getElementById('button');
-  
-  button.onclick = function() {
-    var result = dice.roll();
-    printNumber(result);
-  };
+var button = document.getElementById('button');
+
+button.onclick = function() {
+var result = dice.roll();
+printNumber(result);
+};
   
   
